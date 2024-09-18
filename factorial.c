0416f64ba297d1ff49537c961f6d0043e4086d86
priyanshu kumar <priyanshukumar3122@outlook.com>
2024-09-18 13:38:59 +0530

    else part added

---
+++ factorial.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main(){
+/*	int n,i;
+	unsigned long long int fact = 1;
+	printf("Enter the number =");
+	scanf("%d", &n);
+	
+	if(n<0)
+	printf("\n Factorial of negative numbers is not DEFINED.");
+	else
+		{
+		for(i=1;i<=n;i+=1){
+			fact = fact*i;
+	}
+	printf("\n factorial of %d = %d",n,fact);
+}
+	*/
+	
+	
+
+int main() {
+    int L,W,H;
+    scanf("%d %d %d",&L, &W, &H);
+    
+       if(W < L || H < L){
+        printf("UPLODE ANOTHER");
+    }
+    else if(H > L && W > L){
+        if(H == W){
+            printf("ACCEPTED");
+        }
+        else
+            printf("CROP IT");
+        }
+    }
+    else {
+    	printf("Hello");
+	}
+    
+    
+    
+}
